a6c7a2660fd1881f85c903c2d0c856a8cc2e65f7
dinara <d.shafigullina@itcodegroup.ru>
2023-04-17 12:30:51 +0500

    добавила отправку письма другому человеку

--- send_mail.py
+++ send_mail.py
@@ -5,6 +5,7 @@ from email.mime.text import MIMEText
 
 def send_email(message):
     sender = os.getenv('email_from')
+    recipient = os.getenv('email_to')
     password = os.getenv('email_password')
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -14,14 +15,14 @@ def send_email(message):
         server.login(sender, password)
         msg = MIMEText(message)
         msg["Subject"] = 'Новый вопрос'
-        server.sendmail(sender, sender, msg.as_string())
+        server.sendmail(sender, recipient, msg.as_string())
         return "The message was sent successfully!"
     except Exception as _ex:
         return f"{_ex}\nCheck your login or password please!"
 
 
 def main():
-    message = 'Пришел новый вопрос. Пожалуйста зайдите в группу операторов'
+    message = 'Пришел новый вопрос. Пожалуйста зайдите в группу операторов https://t.me/testtesttestqwerty4454'
     print(send_email(message=message))
 
 
